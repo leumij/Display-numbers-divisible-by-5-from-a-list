955c0248b5744be055f333e15c808634eb25c83a
leumij <cjimuel74@gmail.com>
2024-01-26 14:49:30 +0800

    divide by 5

--- divisiblenumber.py
+++ divisiblenumber.py
@@ -1,6 +1,15 @@
 # Iterate the given list of numbers and print only those numbers which are divisible by 5
 
 # Pseudocode
+
 # Given List
+num_list = [10, 20, 33, 46, 55]
+print("Given list:", num_list)
+print('Divisible by 5:')
+
 # Divide by 5
+for num in num_list:
+    if num % 5 == 0:
+
 # Print the result
+        print(num)  
